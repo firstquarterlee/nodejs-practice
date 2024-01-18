dec1f768115727901d6653fee49f0d636eb6ffbe
firstquarterlee <firstquarterlee@gmail.com>
2024-01-18 21:44:07 +0900

    라우팅

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ app.listen(8080, () => {
     console.log("http://localhost:8080 에서 서버 실행중")
 }) //서버 띄우는 코드임
 
-app.get('/', (요청, 응답) => {
-    응답.send('반갑다')
+app.get('/first', (요청, 응답) => {
+    응답.send("반갑다")
 }) //간단한 서버기능임
+
+app.get('/news', (요청, 응답) => {
+    응답.send("오늘 비옴")
+})
+
+app.get('/shop', (요청, 응답) => {
+    응답.send("쇼핑페이지입니다.")
+})
+
+app. get('/', function (요청, 응답) {
+    응답.sendFile(__dirname + '/index.html')
+                //server.js 담긴 폴더
+})
